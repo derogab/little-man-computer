de967344c8ad870073cd218c610f3a0578b0355b
dlcgold <dlcgold96@gmail.com>
2018-11-22 17:28:52 +0100

    Sistemato fino a branchifpositive

--- Prolog/progetto.pl
+++ Prolog/progetto.pl
@@ -7,16 +7,15 @@
 */
 %pointer valore già tranciato 
 
-addizione(Acc, Pointer, X, flag) :- randseq(99, 999, Mem), % memoria random
-                                    nth0(Pointer, Mem, Value, _),
+addizione(Acc, Pointer, Mem, X, flag) :- nth0(Pointer, Mem, Value, _),
                                     Y is Acc+Value,
                                     X is ((Acc+Value)mod 1000),
-                                    Y > 999.
-addizione(Acc, Pointer, X, noflag) :- randseq(99, 999, Mem), % memoria random
-                                    nth0(Pointer, Mem, Value, _),
-                                    Y is Acc+Value,
-                                    X is ((Acc+Value)mod 1000),
-                                    Y < 1000.
+                                    Y > 999,!.
+addizione(Acc, Pointer, Mem, X, noflag) :- nth0(Pointer, Mem, Value, _),
+                                      Y is Acc+Value,
+                                      X is ((Acc+Value)mod 1000),
+                                      Y < 1000.
+
 
 /**
  * Sottrazione
@@ -24,16 +23,14 @@ addizione(Acc, Pointer, X, noflag) :- randseq(99, 999, Mem), % memoria random
  * Value: 2xx
 */
 
-sottrazione(Acc, Pointer, X, flag) :- randseq(99, 999, Mem), % memoria random
-                                      nth0(Pointer, Mem, Value, _),
+sottrazione(Acc, Pointer, Mem, X, flag) :- nth0(Pointer, Mem, Value, _),
                                       Y is Acc-Value,
                                       X is ((Acc-Value)mod 1000),
-                                      Y < 0.
-sottrazione(Acc, Pointer, X, noflag) :- randseq(99, 999, Mem), % memoria random
-                                        nth0(Pointer, Mem, Value, _),
+                                      Y < 0,!.
+sottrazione(Acc, Pointer, Mem, X, noflag) :- nth0(Pointer, Mem, Value, _),
                                         Y is Acc-Value,
                                         X is ((Acc-Value)mod 1000),
-                                        Y > -1.
+                                        Y >= 0.
 
 /**
  * Store
@@ -74,7 +71,7 @@ branch(Pc, Pointer) :- Pc is Pointer.
 
 branchifzero(Pc, Acc, Pointer, Flag) :- Acc = 0,
                                         Flag = noflag,
-                                        branch(Pc, Pointer).
+                                        Pc is Pointer.
 
 /**
  * Branch if positive
@@ -83,7 +80,7 @@ branchifzero(Pc, Acc, Pointer, Flag) :- Acc = 0,
 */
 
 branchifpositive(Pc, Pointer, Flag) :- Flag = noflag,
-                                       branch(Pc, Pointer).
+                                       Pc is Pointer.
 
 /**
  * Input
@@ -121,32 +118,73 @@ state(Acc, Pc, Mem, In, Out, Flag) :-
     member(Acc, L), % 0-99 check 
     member(Pc, L), % 0-99 check
     proper_length(Mem, MemLength),
-    MemLength < 99, % mem lunga massimo 99
-    max_member(Max, Mem),
-    Max < 999, % massimo minore di 999
-    numlist(0, 999, In), % lista da 0 a 999
-    numlist(0, 999, Out), % lista da 0 a 999
+    MemLength < 100, % mem lunga massimo 99
+    MemLength >= 0,
+    max_member(MaxMem, Mem),
+    MaxMem < 1000, % massimo minore di 999
+    max_member(MinMem, Mem),
+    MinMem >= 0, 
+    max_member(MaxIn, In),
+    MaxIn< 1000,
+    min_member(MinIn, In),
+    MinIn >= 0, 
+    proper_length(In, InLength),
+    InLength >= 0,
+    max_member(MaxOut, Out),
+    MaxOut< 1000,
+    max_member(MinOut, Out),
+    MinOut >= 0, 
+    proper_length(Out, OutLength),
+    OutLength >= 0,
     member(Flag,  [flag, noflag]).
 
 /**
  * One Instruction
 */
-one_instruction(
 
-    state(Acc, Pc, Mem, In, Out, Flag),
-    state(Acc2, Pc2, Mem2, In2, Out2, FLag2)
+istruzione(Pc, Mem , I) :- nth0(Pc, Mem, I, _).
 
-) :- istruzione(Pc, Mem , Istr),
-     addizione(Acc, Istr, Acc2).
-     sottrazione(Acc, Istr, Acc2),
-     store(Acc, Istr, Mem2),
-     load(Acc, Istr, Mem2),
-     branch(Pc, Istr) ,
-     branchifzero(Pc, Acc, Istr) ,
-     branchifpositive(Pc, Istr, Flag) ,
-     input(Acc, In, Istr, NewIn) ,
-     output(Acc, Out, Istr, NewOut).
-
+                           
+one_instruction(state(Acc, Pc, Mem, In, Out, Flag),
+                  state(Acc2, Pc2, Mem2, In2, Out2, Flag2)) :- 
+    istruzione(Pc, Mem , Istr),
+    cellarisultato(Istr, Pointer),
+    (
+        Istr >= 100, Istr < 199 -> addizione(Acc, Pointer, Mem, Acc2, Flag2),
+                                Pc2 is Pc+1,
+                                append([], Mem, Mem2),
+                                append([], In, In2),
+                                append([], Out, Out2);
+        Istr >=200, Istr < 299 -> sottrazione(Acc, Pointer, Mem, Acc2, Flag2),
+                                Pc2 is Pc+1,
+                                append([], Mem, Mem2),
+                                append([], In, In2),
+                                append([], Out, Out2);
+        Istr >= 300, Istr < 400 -> store(Acc, Pointer, Mem, Mem2),
+                                Acc2 is Acc,
+                                Pc2 is Pc+1,
+                                append([], In, In2),
+                                append([], Out, Out2),
+                                copy_term(Flag, Flag2);
+        Istr >= 400, Istr < 500 -> notfound(Pointer);
+        Istr >= 500, Istr < 600 -> branch(Pc2, Pointer),
+                                   Acc2 is Acc,
+                                   append([], Mem, Mem2),
+                                   append([], In, In2),
+                                   append([], Out, Out2),
+                                   copy_term(Flag, Flag2);
+        Istr >= 600, Istr < 700 -> branchifzero(Pc2, Acc, Pointer, Flag),
+                                   Acc2 is Acc,
+                                   append([], Mem, Mem2),
+                                   append([], In, In2),
+                                   append([], Out, Out2);
+        Istr >= 700, Istr < 800 -> branchifpositive(Pc2, Pointer, Flag),
+                                   Acc2 is Acc,
+                                   append([], Mem, Mem2),
+                                   append([], In, In2),
+                                   append([], Out, Out2)                          
+                                  
+    ).
 
 
 %---------------------------------------------
@@ -170,4 +208,6 @@ one_instruction(
 cellarisultato(I, X) :- number_chars(I, L),
                         nth0(0, L, _, L2),
                         number_chars(X, L2).
+
+
 
